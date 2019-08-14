b93a6a2ca68d327f045cfad92c67bc6c6975e9e5
erik.wang <erik.wang@ximalaya.com>
2019-08-14 22:17:30 +0800

    :q
    :q

---
+++ src/main/java/study/erik/algorithm/classic/SquareRootCalculations.java
@@ -0,0 +1,53 @@
+package study.erik.algorithm.classic;
+
+/**
+ * @Date 2019-08-14
+ * @Created by erik
+ * 二分法和牛顿法开根号
+ * 参考文章 https://www.cnblogs.com/wangkundentisy/p/8118007.html
+ *
+ */
+public class SquareRootCalculations {
+
+    public static double solutionByBisectionMethod(double n) {
+
+        double precision = Math.exp(-6);
+
+        double low = 0;
+        double high = n;
+        double mid = (low + high) / 2;
+
+        while (Math.abs(mid * mid - n) > precision) {
+
+            double diff = mid * mid - n;
+            if (diff > 0) {
+                high = mid;
+            } else if (diff < 0) {
+                low = mid;
+            } else {
+                return mid;
+            }
+            mid = (low + high) / 2;
+        }
+
+        return mid;
+    }
+
+
+    public static double solutionByNewtonsMethod(double n) {
+
+        double result = 1;
+        double precision = Math.exp(-6);
+
+        while (Math.abs(result * result - n) > precision) {
+            result = 0.5 * (result + n / result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solutionByNewtonsMethod(82));
+        System.out.println(solutionByBisectionMethod(82));
+    }
+
+}
