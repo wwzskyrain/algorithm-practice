7e0ee778bf3b92818828c4e3a8317e00d83a157e
yueyi <weizhen.wangweizhe@alibaba-inc.com>
2021-12-20 08:49:53 +0800

    基数排序

---
+++ src/main/java/study/erik/algorithm/leetcode/hard/MaximumGap.java
@@ -0,0 +1,61 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2021 All Rights Reserved.
+ */
+package study.erik.algorithm.leetcode.hard;
+
+import study.erik.algorithm.util.LetCodeCommit;
+
+/**
+ * @author yueyi
+ * @version : MaximumGap.java, v 0.1 2021年12月20日 8:31 上午 yueyi Exp $
+ */
+public class MaximumGap {
+
+    @LetCodeCommit(title = "164. Maximum Gap")
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        // m is the maximal number in nums
+        int m = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            m = Math.max(m, nums[i]);
+        }
+
+        int exp = 1; // 1, 10, 100, 1000 ...
+        int R = 10; // 10 digits
+
+        int[] aux = new int[nums.length];
+
+        while (m / exp > 0) { // Go through all digits from LSB to MSB
+            int[] count = new int[R];
+
+            for (int i = 0; i < nums.length; i++) {
+                count[(nums[i] / exp) % 10]++;
+            }
+
+            for (int i = 1; i < count.length; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                aux[--count[(nums[i] / exp) % 10]] = nums[i];
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = aux[i];
+            }
+            exp *= 10;
+        }
+
+        int max = 0;
+        for (int i = 1; i < aux.length; i++) {
+            max = Math.max(max, aux[i] - aux[i - 1]);
+        }
+
+        return max;
+    }
+
+}
