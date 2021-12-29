9a6d2c2c0aafe71c0ef0f033dea39003eb92b1a5
yueyi <weizhen.wangweizhe@alibaba-inc.com>
2021-12-29 10:17:08 +0800

    第k个排列数

--- src/main/java/study/erik/algorithm/leetcode/backtracking/PermutationSequence.java
+++ src/main/java/study/erik/algorithm/leetcode/backtracking/PermutationSequence.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import study.erik.algorithm.util.LetCodeCommit;
 
+import java.util.ArrayList;
+
 /**
  * @author erik.wang
  * @date 2020-09-05 08:35
@@ -11,13 +13,30 @@ import study.erik.algorithm.util.LetCodeCommit;
 public class PermutationSequence {
 
     @LetCodeCommit(title = "60. Permutation Sequence", diff = "m",
-            selfRemark = "回溯法，不排序的，但是成绩不好。然后我们写一个排序的来")
+            selfRemark = "回溯法，不排序的，但是成绩不好。然后我们写一个排序的来。"
+                    + "回溯法是一个暴力解法")
     public String getPermutation(int n, int k) {
-        return solution(new boolean[n], new int[]{k}, new char[n], 0);
+        return solution(new boolean[n], new int[] {k}, new char[n], 0);
+    }
+
+    public String solution2(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> num = new ArrayList<>();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            num.add(i);
+        }
+        for (int i = 0, l = k - 1; i < n; i++) {
+            fact /= (n - i);
+            int index = (l / fact);
+            sb.append(num.remove(index));
+            l -= index * fact;
+        }
+        return sb.toString();
     }
 
     private String solution(boolean[] nums, int[] k, char[] num, int curIndex) {
-
         if (curIndex == nums.length) {
             if (k[0] == 1) {
                 return String.valueOf(num);
@@ -50,8 +69,7 @@ public class PermutationSequence {
     @Test
     public void test_solution_2() {
         int n = 4, k = 9;
-        Assert.assertEquals("2314", getPermutation(n, k));
+        Assert.assertEquals("2314", solution2(n, k));
     }
-
 
 }
