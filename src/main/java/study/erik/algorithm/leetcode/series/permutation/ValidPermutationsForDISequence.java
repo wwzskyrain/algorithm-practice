e278ef18f41f1891143ce62740af1be7f460407c
yueyi <weizhen.wangweizhe@alibaba-inc.com>
2022-10-10 20:03:22 +0800

    d

---
+++ src/main/java/study/erik/algorithm/leetcode/series/permutation/ValidPermutationsForDISequence.java
@@ -0,0 +1,22 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package study.erik.algorithm.leetcode.series.permutation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import study.erik.algorithm.util.LetCodeCommit;
+
+/**
+ * @author yueyi
+ * @version : ValidPermutationsForDISequence.java, v 0.1 2022年10月09日 22:24 yueyi Exp $
+ */
+@RunWith(Parameterized.class)
+public class ValidPermutationsForDISequence {
+
+    @LetCodeCommit(title = "903. Valid Permutations for DI Sequence")
+    public int numPermsDISequence(String s) {
+        
+    }
+}
